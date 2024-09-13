3b9794ee8da178086cb43c50313fb368e1f0188f
Jadilson Lucio dos Santos <jadilson1995@hotmail.com>
2024-09-13 18:48:46 -0300

    first commit

---
+++ src/components/ui/label.tsx
@@ -0,0 +1,14 @@
+import type { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+export function Label(props: ComponentProps<'label'>) {
+  return (
+    <label htmlFor="js_id" aria-label="A label"
+      {...props}
+      className={twMerge(
+        'font-medium text-sm tracking-tight leading-normal',
+        props.className
+      )}
+    />
+  )
+}
